b6a39317e07d7cb439776862497430242443a638
Yazeed <yazxdo77@gmail.com>
2026-09-01 09:35:06 +0530

    update assignment3.py

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,1 +1,16 @@
+# To check whether a triangle is right-angled or not
 
+print("RIGHT ANGLED TRIANGLE")
+
+def check(a, b, c):
+    if a * a + b * b == c * c:
+        print("It is a right-angled triangle")
+    else:
+        print("It is not a right-angled triangle")
+
+
+a = int(input("Enter first side:"))
+b = int(input("Enter second side:"))
+c = int(input("Enter third side:"))
+
+check(a, b, c)
