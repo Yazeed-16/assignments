11731bb13f4eaf77e8ead9cdb17728481d76f5d9
Yazeed <yazxdo77@gmail.com>
2026-09-01 09:22:39 +0530

    Update assignment2.py

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,1 +1,14 @@
+# To find largest of three numbers
 
+print("LARGEST OF THREE NUMBERS")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a > b and a > c:
+    print("Largest number is:", a)
+elif b > a and b > c:
+    print("Largest number is:", b)
+else:
+    print("Largest number is:", c)
